c6f3b3aa210da2d28cc7887112575dc0b0e136e8
ralvarezb13 <ramonalvarezbaeza@gmail.com>
2024-10-24 15:08:05 -0600

    Test capacitor

---
+++ android/app/src/main/java/com/palaceresorts/palaceapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.palaceresorts.palaceapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.palaceresorts.palaceapp',
+  appName: 'base-project-pwa',
+  webDir: 'dist'
+};
+
+export default config;
