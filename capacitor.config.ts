a3b66f80e067b15ab14e7ce1ce2ced156e60ee57
ralvarezb13 <ramonalvarezbaeza@gmail.com>
2024-10-24 15:20:01 -0600

    test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.palaceresorts.palaceapp',
-  appName: 'base-project-pwa',
-  webDir: 'dist'
+  appId: "com.miempresa.mipwa",
+  appName: "Mi PWA",
+  webDir: "www",
+  server: {
+    url: "https://develop.d3khydlw98663i.amplifyapp.com/",
+    cleartext: true,
+  },
 };
 
 export default config;
